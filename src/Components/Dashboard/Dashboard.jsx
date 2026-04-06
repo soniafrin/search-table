c68971e0e0aa154d4c353872b531e63917e3e412
soniafrin <soniafrin50@gmail.com>
2026-04-06 17:31:21 +0100

    removed item from dashboard

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -2,19 +2,22 @@
 
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
+
 
 const Dashboard = () =>{
     const [allOrders, setAllOrders] = useState([]);
     // console.log(allOrders);
     const [filter, setFilter] = useState("");
     useEffect(()=>{
-        const interval = setInterval(() =>{
+        // const interval = setInterval(() =>{
             const existingOrders = JSON.parse(localStorage.getItem('existingOrderNums')) || [];
             setAllOrders(existingOrders);
-        }, 3000);
+            // console.log(existingOrders);
+        // }, 9000);
 
         
-        return () => clearInterval(interval)
+        // return () => clearInterval(interval)
         
     }, []);
 
@@ -23,10 +26,16 @@ const Dashboard = () =>{
 
         // console.log(filtedtedOrders);
 
-
+const handleMarkOrder = (ind) =>{
+    console.log(ind);
+    const removeMarkedOrder = allOrders.filter((order, index) => index !== ind)
+    setAllOrders(removeMarkedOrder)
+    toast.warning("order is removed")
+    // console.log(removeMarkedOrder);
+}
     return(
 
-        <div className="w-6/12 mx-auto text-center mt-22 space-y-5 rounded-xl p-5 shadow-lg bg-[#F5EFE0]">
+        <div className="  lg:w-6/12 mx-auto text-center space-y-5 rounded-xl p-5 shadow-lg bg-[#F5EFE0]">
             <h1 className="text-2xl font-bold">Dashboard Page</h1>
             <input 
             className="bg-gray-200 border border-gray-500 py-2 px-3 rounded-xl"
@@ -48,7 +57,8 @@ const Dashboard = () =>{
                 (
                     <ul className="space-y-1 flex flex-wrap justify-center gap-2  ">
                         {
-                            allOrders.map((order, index )=> (<li className="h-15 bg-[#1F1611] border border-gray-500 py-1 w-60 rounded-xl text-[white] flex items-center justify-center" key={index}><input type="checkbox" /> Order No. {order.orderNumber} - Table No. {order.table} </li>))
+                            allOrders.map((order, index )=> (<li onClick={()=>handleMarkOrder(index)} className="cursor-pointer h-15 bg-[#1F1611] border border-gray-500 py-1 w-60 rounded-xl text-[white] flex items-center justify-center" key={index}>
+                                 Order No. {order.orderNumber} - Table No. {order.table}</li>))
                         }
                     </ul>
                 )}
